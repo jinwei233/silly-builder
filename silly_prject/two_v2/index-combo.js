3de5188e021726b6657be4ca5f857ec650778361
cookieu <cookieu@gmail.com>
2013-01-04 13:55:34 +0800

    新版的silly_builder

--- silly_prject/two_v2/index-combo.js
+++ silly_prject/two_v2/index-combo.js
@@ -2,13 +2,17 @@
  *gen by sb
  *combo files:
 
+/home/tom/Dropbox/gits/silly_builder/silly_prject/three/mods/modc.js
 /home/tom/Dropbox/gits/silly_builder/silly_prject/two_v2/mods/modc.js
 /home/tom/Dropbox/gits/silly_builder/silly_prject/two_v2/mods/modb.js
 /home/tom/Dropbox/gits/silly_builder/silly_prject/two_v2/mods/moda.js
 /home/tom/Dropbox/gits/silly_builder/silly_prject/two_v2/index.js
 
 */
-KISSY.add("silly_prject/two_v2/mods/modc",function(){
+KISSY.add("silly_prject/three/mods/modc",function(){
+
+});
+;KISSY.add("silly_prject/two_v2/mods/modc",function(){
 
 },{
   requires:["silly_prject/three/mods/modc"]
